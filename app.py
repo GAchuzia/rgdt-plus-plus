3f783893ad4e821f55925843356f49100b490f44
Matteo Golin <matteo.golin@gmail.com>
2023-11-18 15:07:44 -0500

    Added CORS>

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 PORT: int = 8080
+UPLOAD_FOLDER: str = "./tests"
+ALLOWED_EXTENSIONS: list[str] = ["xml", "json"]
 app = Flask(__name__, template_folder="web", static_folder="web")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# Allow cross-origin resource sharing for simplicity
+CORS(app)
 
 
 @app.route("/")
