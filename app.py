e5edc657adf55a48a7a04f016999a1403c3ee593
Matteo Golin <matteo.golin@gmail.com>
2023-11-18 15:36:19 -0500

    Almost working file upload lol.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
+import os
 
 PORT: int = 8080
 UPLOAD_FOLDER: str = "./tests"
@@ -11,6 +13,11 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 CORS(app)
 
 
+def allowed_file(filename: str):
+    """Checks if file is of an allowed type."""
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -19,6 +26,26 @@ def home():
 @app.route("/output")
 def output():
     return render_template("output.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    # check if the post request has the file part
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+
+    file = request.files["file"]
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == "":
+        flash("No selected file")
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return redirect(url_for("download_file", name=filename))
 
 
 if __name__ == "__main__":
--- web/index.js
+++ web/index.js
@@ -1,2 +1,8 @@
-
-
+function upload_file(){
+    var myUploadedFile = document.getElementById("uploadFile").files[0];
+    const res = await axios.post('/upload', formData, {
+        headers: {
+            'Content-Type': 'file'
+        }
+    });
+}
